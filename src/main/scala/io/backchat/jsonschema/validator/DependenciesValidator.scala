7175e66713ecf15a839f7a136d36d3ca9c993fed
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-05-19 23:43:58 +0200

    small tweak

--- src/main/scala/io/backchat/jsonschema/validator/DependenciesValidator.scala
+++ src/main/scala/io/backchat/jsonschema/validator/DependenciesValidator.scala
@@ -8,8 +8,11 @@ import Json._
 class DependenciesValidator extends SchemaValidator {
   val property: String = "dependencies"
 
-  def validateSyntax(value: Json.JValue): Validation[ValidationError, Json.JValue] =
-    value.success
+  def validateSyntax(value: Json.JValue): Validation[ValidationError, Json.JValue] = value \ property match {
+    case _: JString | _: JArray | JNull | JUndefined => value.success
+    case m: JObject if JsonSchema.validateSyntax(m).isSuccess => value.success
+    case _ => ValidationError("The dependencies property is invalid", property).fail
+  }
 
   def validateValue(fieldName: String, value: Json.JValue, schema: Json.JValue): Validation[ValidationError, Json.JValue] =
     value.success
